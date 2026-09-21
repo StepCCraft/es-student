7eb1cad07e9dd06787e7048ac23a41672f232946
Жарков Василий <zvasil1801@gmail.com>
2026-09-21 16:05:57 +0300

    Сделана п1.2.3

---
+++ 123-led-button-debounce/main.c
@@ -0,0 +1,53 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/regs/addressmap.h"
+#include "hardware/regs/sio.h"
+
+// объявляем константу вывода светодиода
+const uint BUTTON_PIN = 15;
+const uint LED_PIN = 25;
+const uint DEBOUNCE_MS = 20;
+
+
+
+
+
+
+
+
+
+bool get_button_debounce(uint pin)
+{
+    bool state = gpio_get(pin);
+    sleep_ms(DEBOUNCE_MS);
+    return state && gpio_get(pin);
+}
+
+
+
+
+int main()
+{
+    // инициализируем пин светодиода
+    // настраиваем пин светодиода на выход
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init(BUTTON_PIN);
+    gpio_pull_up(BUTTON_PIN);
+
+    bool led = false;
+    bool previous = false;
+
+    while (1)
+    {
+        bool current = get_button_debounce(BUTTON_PIN);
+
+        if (previous == true && current == false)
+        {
+            led = !led;
+            gpio_put(LED_PIN, led);
+        }
+
+        previous = current;
+    }
+}
