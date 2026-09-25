1077019b90f97b898e6484dab7ed62eea1417295
Жарков Василий <zvasil1801@gmail.com>
2026-09-25 15:38:11 +0300

    Сделана п1.3.1

--- 123-led-button-debounce/main.c
+++ 123-led-button-debounce/main.c
@@ -6,6 +6,14 @@
 // объявляем константу вывода светодиода
 const uint BUTTON_PIN = 15;
 const uint LED_PIN = 25;
+const uint DEBOUNCE_MS = 20;
+bool get_button_debounce(uint pin)
+{
+    bool state = gpio_get(pin);
+    sleep_ms(DEBOUNCE_MS);
+    return state && gpio_get(pin);
+}
+
 int main()
 {
     // инициализируем пин светодиода
@@ -18,11 +26,9 @@ int main()
     gpio_pull_up(LED_PIN);
     bool led = false;
     bool previous = false;
-
     while (1)
     {
-        bool current = gpio_get(BUTTON_PIN);
-
+        bool current = get_button_debounce(BUTTON_PIN);
         if (previous == true && current == false)
         {
             led = !led;
